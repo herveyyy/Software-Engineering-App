0d4226a5ed48236872a8ecab939e2b1bc55cfad7
herveyyy <captainhervey@gmail.com>
2023-03-12 20:23:39 +0800

    hehe

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,12 +41,15 @@ function Button({ text, bg, padding }) {
 
 function Navbar() {
   return (
-    <div className=" z-40 fixed left-0 right-0 top-0 h-16 shadow-md  border-gray-100 bg-gray-900">
+    <div className=" z-40 fixed left-0 right-0 top-0 h-16 shadow-md border-gray-100 bg-gray-900">
       <nav className="flex items-center container mx-auto h-full justify-between">
-        <h1 className="font-semibold uppercase text-lg text-gray-200">
+        <div className=""> 
+          <h1 className=" font-semibold uppercase text-lg text-gray-200">
           🤓 SOFTWARE ENGINEER
         </h1>
-        <div>
+        </div>
+       
+        <div >
           <ul className="flex items-center space-x-10 text-sm">
             <li>
               <Link to="/" className="text-gray-400 hover:text-gray-100">
